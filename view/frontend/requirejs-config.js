c9ce8ecbe0ce4e18c0aa6300b29ee0479e21e847
andyexeter <palmer.andy@gmail.com>
2018-04-17 10:35:46 +0100

    Fix owl carousel requirejs loading issues

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,15 +1,10 @@
 var config = {
-    // map: {
-    //     '*': {
-    //         'owlcarousel': 'Mageplaza_BetterSlider/js/owl.carousel',
-    //     },
-    // },
-    // paths: {
-    //     'owlcarousel': 'Mageplaza_BetterSlider/js/owl.carousel',
-    // },
-    // shim: {
-    //     'owlcarousel': {
-    //         deps: ['jquery']
-    //     }
-    // }
+    paths: {
+        'owlcarousel': "Mageplaza_BetterSlider/js/owl.carousel"
+    },
+    shim: {
+        'owlcarousel': {
+            deps: ['jquery']
+        }
+    }
 };
