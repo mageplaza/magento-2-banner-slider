9ab4902a7ea2253149bda59ff34ae38825768f0c
bdtrungit <bdtrungit@gmail.com>
2019-02-22 16:36:11 +0700

    validate responsive value

--- Block/Adminhtml/Config/Field/Responsive.php
+++ Block/Adminhtml/Config/Field/Responsive.php
@@ -34,8 +34,8 @@ class Responsive extends AbstractFieldArray
      */
     protected function _prepareToRender()
     {
-        $this->addColumn('size', ['label' => __('Screen size from'), 'renderer' => false]);
-        $this->addColumn('items', ['label' => __('Number of items'), 'renderer' => false]);
+        $this->addColumn('size', ['label' => __('Screen size from'), 'renderer' => false, 'class' => 'required-entry validate-digits']);
+        $this->addColumn('items', ['label' => __('Number of items'), 'renderer' => false, 'class' => 'required-entry validate-digits']);
 
         $this->_addAfter       = false;
         $this->_addButtonLabel = __('Add');
