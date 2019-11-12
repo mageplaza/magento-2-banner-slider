e44f0fb55addb8de996c4de64f62299e96315c99
Mekkaoui <medmek@users.noreply.github.com>
2019-11-12 19:20:17 +0100

    add IdentityInterface to Banner Model
    Banner model should implement IdentityInterface in order to get the cache invalidation in this model (eventhough  $_cacheTag and function getIdentities() are implemented already (see vendor/magento/framework/App/Cache/Tag/Strategy/Identifier.php:24)

--- Model/Banner.php
+++ Model/Banner.php
@@ -26,6 +26,7 @@ use Magento\Framework\Model\AbstractModel;
 use Magento\Framework\Model\Context;
 use Magento\Framework\Model\ResourceModel\AbstractResource;
 use Magento\Framework\Registry;
+use Magento\Framework\DataObject\IdentityInterface;
 use Mageplaza\BannerSlider\Model\Config\Source\Image as configImage;
 use Mageplaza\BannerSlider\Model\ResourceModel\Slider\Collection;
 use Mageplaza\BannerSlider\Model\ResourceModel\Slider\CollectionFactory as sliderCollectionFactory;
@@ -55,7 +56,7 @@ use Mageplaza\BannerSlider\Model\ResourceModel\Banner as ResourceBanner;
  * @method Banner setAffectedSliderIds(array $ids)
  * @method bool getAffectedSliderIds()
  */
-class Banner extends AbstractModel
+class Banner extends AbstractModel implements IdentityInterface
 {
     /**
      * Cache tag
