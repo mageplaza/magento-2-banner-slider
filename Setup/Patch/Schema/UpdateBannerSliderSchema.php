b47aaa1e9df0961b4d18b021ba0c9a94d61ebdfe
NamMasu <namvq@mageplaza.vn>
2025-03-05 16:19:13 +0700

    - fix issue when setup upgrade with db_schema

--- Setup/Patch/Schema/UpdateBannerSliderSchema.php
+++ Setup/Patch/Schema/UpdateBannerSliderSchema.php
@@ -4,55 +4,79 @@ namespace Mageplaza\BannerSlider\Setup\Patch\Schema;
 use Exception;
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\Filesystem;
-use Magento\Framework\Setup\Patch\SchemaPatchInterface;
 use Mageplaza\BannerSlider\Model\Config\Source\Template;
 use Psr\Log\LoggerInterface;
+use Magento\Framework\Setup\Patch\SchemaPatchInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
 
-class CopyDemoImages implements SchemaPatchInterface
+class UpdateBannerSliderSchema implements SchemaPatchInterface
 {
+    /**
+     * @var SchemaSetupInterface
+     */
+    private SchemaSetupInterface $schemaSetup;
     /**
      * @var Filesystem
      */
     protected Filesystem $fileSystem;
-
     /**
      * @var Template
      */
     protected Template $template;
-
     /**
      * @var LoggerInterface
      */
     protected LoggerInterface $logger;
 
     /**
-     * CopyDemoImages constructor.
-     *
      * @param Filesystem $filesystem
      * @param Template $template
      * @param LoggerInterface $logger
+     * @param SchemaSetupInterface $schemaSetup
      */
     public function __construct(
         Filesystem $filesystem,
         Template $template,
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        SchemaSetupInterface $schemaSetup
     ) {
         $this->fileSystem = $filesystem;
         $this->template = $template;
         $this->logger = $logger;
+        $this->schemaSetup = $schemaSetup;
     }
 
     /**
-     * @return $this
+     * @return $this|UpdateBannerSliderSchema
      */
     public function apply()
     {
+        $setup = $this->schemaSetup;
+        $setup->startSetup();
+
+        $connection = $setup->getConnection();
+        $tableName = $setup->getTable('mageplaza_bannerslider_banner_slider');
+
+        $indexList = $connection->getIndexList($tableName);
+        $indexesToRemove = [
+            'MAGEPLAZA_BANNERSLIDER_BANNER_SLIDER_SLIDER_ID',
+            'MAGEPLAZA_BANNERSLIDER_BANNER_SLIDER_BANNER_ID',
+            'MAGEPLAZA_BANNERSLIDER_BANNER_SLIDER_UNIQUE'
+        ];
+
+        foreach ($indexesToRemove as $indexName) {
+            if (isset($indexList[$indexName])) {
+                $connection->dropIndex($tableName, $indexName);
+            }
+        }
+
         $this->copyDemoImage();
+        $setup->endSetup();
         return $this;
     }
 
     /**
-     * Copy demo images
+     * @return void
      */
     private function copyDemoImage()
     {
